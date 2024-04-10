a911c87ef0bbfb05a51a97fc38c5f0f5bcbf065a
Tahsin0909 <tajisan789@gmail.com>
2024-04-10 16:34:28 +0600

    [location](home) Get location in home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,50 @@
+import React, { useState } from 'react';
+interface Location {
+    latitude: number;
+    longitude: number;
+}
+interface Error {
+    error: string
+}
 const Home = () => {
+    const [userLocation, setUserLocation] = useState<Location | null>(null)
+    const [error, setError] = useState<Error | null>(null)
+
+    const getUserLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const { latitude, longitude } = position.coords;
+                    setUserLocation({ latitude, longitude });
+                },
+                (error) => {
+                    // display an error if we cant get the users position
+                    setError({ error: 'Error getting user location' });
+                    setUserLocation({ latitude: 22.739 , longitude:90.1232 });
+                }
+            )
+        }
+        else {
+            console.error('Geolocation is not supported by this browser.');
+            setUserLocation({ latitude: 22.739 , longitude:90.1232 });
+        }
+    };
+    if(!userLocation){
+        getUserLocation()
+    }
     return (
         <div>
-            <p>Main Home</p>
+            <button className='btn' onClick={() => getUserLocation()}>Location</button>
+            {
+                error && <p>{error.error}</p>
+            }
+            {userLocation && (
+                <div>
+                    <h2>User Location</h2>
+                    <p>Latitude: {userLocation.latitude}</p>
+                    <p>Longitude: {userLocation.longitude}</p>
+                </div>
+            )}
         </div>
     );
 };
